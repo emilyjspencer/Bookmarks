eac8903ff83b291f7449447baebcf44eb8dc6ecf
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-05-03 15:01:34 +0100

    Creates new table - comments, which references the bookmarks table. One to many relationship between a bookmark and comments. A bookmarks can have many comments. A comment belongs to a bookmark. Truncates the comments table also before each test

--- spec/wipe_test_database.rb
+++ spec/wipe_test_database.rb
@@ -6,5 +6,5 @@ require 'pg'
 
    connection = PG.connect(dbname: 'bookmark_manager_new_test') 
 
-   connection.exec("TRUNCATE bookmark_manager_new_test;") 
+   connection.exec("TRUNCATE bookmarks, comments;") 
  end
